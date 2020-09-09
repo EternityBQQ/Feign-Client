2541f7769a5466fbdd15a10b6243989699ac4530
zhangzheng521 <2215751025@qq.com>
2020-09-09 19:08:02 +0800

    【FIX】文档服务

--- src/main/java/com/itcast/feignclient/config/Swagger2Config.java
+++ src/main/java/com/itcast/feignclient/config/Swagger2Config.java
@@ -2,6 +2,7 @@ package com.itcast.feignclient.config;
 
 import com.github.xiaoymin.knife4j.spring.annotations.EnableKnife4j;
 import com.google.common.base.Predicates;
+import com.google.common.collect.Lists;
 import com.itcast.feignclient.pojo.SwaggerProperty;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.context.annotation.Bean;
@@ -11,11 +12,13 @@ import springfox.bean.validators.configuration.BeanValidatorPluginsConfiguration
 import springfox.documentation.builders.ApiInfoBuilder;
 import springfox.documentation.builders.PathSelectors;
 import springfox.documentation.builders.RequestHandlerSelectors;
-import springfox.documentation.service.ApiInfo;
-import springfox.documentation.service.Contact;
+import springfox.documentation.service.*;
 import springfox.documentation.spi.DocumentationType;
+import springfox.documentation.spi.service.contexts.SecurityContext;
 import springfox.documentation.spring.web.plugins.Docket;
 import springfox.documentation.swagger2.annotations.EnableSwagger2;
+
+import java.util.List;
 
 /**
  * @author zheng.zhang
@@ -45,7 +48,31 @@ public class Swagger2Config {
                 .paths(PathSelectors.any())
                 // 错误路径不监控
                 .paths(Predicates.not(PathSelectors.regex("/error.*")))
+                .build()
+                .securityContexts(Lists.newArrayList(securityContext()))
+                .securitySchemes(Lists.<SecurityScheme>newArrayList(apiKey()));
+    }
+
+    /**
+     * 安全上下文。即存储认证授权的相关信息，实际上就是存储"当前用户"账号信息和相关权限
+     * @return SecurityContext
+     */
+    private SecurityContext securityContext() {
+        return SecurityContext.builder()
+                .securityReferences(defaultAuth())
+                .forPaths(PathSelectors.regex("/.*"))
                 .build();
+    }
+
+    private List<SecurityReference> defaultAuth() {
+        AuthorizationScope authorizationScope = new AuthorizationScope("global", "accessEverything");
+        AuthorizationScope[] authorizationScopes = new AuthorizationScope[1];
+        authorizationScopes[0] = authorizationScope;
+        return Lists.newArrayList(new SecurityReference("TOKEN", authorizationScopes));
+    }
+
+    private ApiKey apiKey() {
+        return new ApiKey("TOKEN", "Authorization", "header");
     }
 
     /**
